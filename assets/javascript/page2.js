d6620920fa5c5f213d5dbd8a320829faa4fa008b
Jobsidney <jobsidney67@gmail.com>
2022-05-14 11:36:28 +0300

    Adding array push funtion to JS file

--- assets/javascript/page2.js
+++ assets/javascript/page2.js
@@ -1,3 +1,20 @@
-const questionAnswers=[
+const question=['Answ1','Answ2','Answ3','Answ4','Answ5','Answ6']
+const checkedBox=[];
+
+
+// function doResults() {
+//     document.getElementById('outResults').innerHTML=23;
     
-]
+// }
+
+function doResults() {
+    for (let i = 1;i<6;i++) {
+        if (document.getElementsByName('Answ${i}').value==true) {
+            checkedBox.push('true');
+            i++;          
+        }    
+    }
+    document.getElementById('outResults').innerHTML=checkedBox.length;
+}
+
+
